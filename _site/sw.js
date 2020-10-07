168e287b59448a84467fed1ca2a7e70080862036
Ta Duc Tung <tatung@Tas-MacBook-Pro-105.local>
2020-10-08 01:25:00 +0900

    post thumbnail

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20201008000420";
+var CACHE_NAME = "saikiransripada-20201008012132";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202010080004",
+        "/css/style.css?202010080121",
         "/"
       ]);
     })
