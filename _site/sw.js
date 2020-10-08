ee79e7ef28650dcb9c78d5d66ae203a692e50265
Ta Duc Tung <tatung@Tas-MacBook-Pro-105.local>
2020-10-08 20:44:09 +0900

    asset img

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20201008163804";
+var CACHE_NAME = "saikiransripada-20201008204350";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202010081638",
+        "/css/style.css?202010082043",
         "/"
       ]);
     })
