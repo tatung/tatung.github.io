85fe4e40c614e9ded46ca0de3b399ec2e9b5c0b6
Ta Duc Tung <tatung@Tas-MacBook-Pro-105.local>
2020-10-08 20:49:51 +0900

    image iot

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20201008204350";
+var CACHE_NAME = "saikiransripada-20201008204901";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202010082043",
+        "/css/style.css?202010082049",
         "/"
       ]);
     })
