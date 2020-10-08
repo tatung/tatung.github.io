23285066ee14663b212ef73ed1ccc90c2e586294
Ta Duc Tung <tatung@Tas-MacBook-Pro-105.local>
2020-10-08 11:38:06 +0900

    remove Netlify admin

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20201008113318";
+var CACHE_NAME = "saikiransripada-20201008113653";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202010081133",
+        "/css/style.css?202010081136",
         "/"
       ]);
     })
