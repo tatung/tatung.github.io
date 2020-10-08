1fb8bccfa347976d9bf589f394740b71a0ec5823
Ta Duc Tung <tatung@Tas-MacBook-Pro-105.local>
2020-10-08 21:41:05 +0900

    myadmin redirect

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20201008205536";
+var CACHE_NAME = "saikiransripada-20201008213939";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202010082055",
+        "/css/style.css?202010082139",
         "/"
       ]);
     })
