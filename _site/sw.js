122cfc18278a031b98b1f4562acfc1231ca120f2
Ta Duc Tung <tatung@Tas-MacBook-Pro-105.local>
2020-10-08 14:15:54 +0900

    Netlify admin

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20201008113653";
+var CACHE_NAME = "saikiransripada-20201008141517";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202010081136",
+        "/css/style.css?202010081415",
         "/"
       ]);
     })
