a7231fbcbe15cd09bd37359159e1a66482ef4a3c
Ta Duc Tung <tatung@Tas-MacBook-Pro-105.local>
2020-10-08 15:30:16 +0900

    post front matter for netlify

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20201008141517";
+var CACHE_NAME = "saikiransripada-20201008152935";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202010081415",
+        "/css/style.css?202010081529",
         "/"
       ]);
     })
