b3ff4f8818d2c1ab8f08c37244ab0746374f5cdd
Ta Duc Tung <tatung@Tas-MacBook-Pro-105.local>
2020-10-09 21:03:39 +0900

    Test fontsize mobile 20px

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20201009201557";
+var CACHE_NAME = "saikiransripada-20201009210147";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202010092015",
+        "/css/style.css?202010092101",
         "/"
       ]);
     })
