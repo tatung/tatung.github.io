972aa82ba4ff9d973c6a341b9c3f0b5dc7f6958d
Ta Duc Tung <tatung@Tas-MacBook-Pro-105.local>
2020-10-09 21:38:17 +0900

    increase font mobile

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20201009210839";
+var CACHE_NAME = "saikiransripada-20201009213704";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202010092108",
+        "/css/style.css?202010092137",
         "/"
       ]);
     })
